7e16408529c583bfdab9992d8d122215b4685ce1
dano03001 <70076208+dano03001@users.noreply.github.com>
2021-01-13 15:18:38 +0100

    Add files via upload

---
+++ Klasser/Klasser/Bil.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+class Bil
+{
+    private string Tillverkare;
+    private string Modell;
+    private int Year;
+    private int Vikt;
+
+
+    public void setTillverkare(string T) 
+    { 
+        Tillverkare = T; 
+    }
+
+    public string getTillverkare() 
+    {
+        return Tillverkare; 
+    }
+
+    public void setModell(string M) 
+    { 
+        Modell = M; 
+    }
+
+    public string getModell() 
+    { 
+        return Modell; 
+    }
+
+    public void setYear(int Y) 
+    { 
+        Year = Y; 
+    }
+
+    public int getYear() 
+    { 
+        return Year; 
+    }
+
+    public void setVikt(int V) 
+    {
+        Vikt = V; 
+    }
+
+    public int getVikt() 
+    { 
+        return Vikt; 
+    }
+}
---
+++ Klasser/Klasser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Bil p = new Bil();
+            p.setTillverkare("Volvo");
+            p.setModell("V70");
+            p.setYear(1996);
+            p.setVikt(2200);
+
+            Console.WriteLine("Tillverkare: " + p.getTillverkare());
+            Console.WriteLine("Modell: " + p.getModell());
+            Console.WriteLine("År: " + p.getYear());
+            Console.WriteLine("Vikt: " + p.getVikt() + " kg");
+        }
+    }
+}
