fd7a77ec189e3db7c4815ec125dae5634cdc926f
dano03001 <70076208+dano03001@users.noreply.github.com>
2021-01-27 15:13:08 +0100

    Add files via upload

---
+++ ConsoleApp46/ConsoleApp46/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<double> tallista = new List<double>();
+            tallista.Add(4.1);
+            tallista.Add(8.5);
+            tallista.Add(6.8);
+            tallista.Add(2.5);
+            tallista.Add(1.6);
+
+            tallista.RemoveAt(3);
+
+            foreach (int element in tallista)
+                Console.WriteLine(element);
+        }
+    }
+}
